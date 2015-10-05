69873578fcd1a8646c3ce4a9c7c54c3d96cf0302
Иван Еленский <elenskivan@hotmail.com>
2015-10-05 21:33:19 +0000

    Done

--- NETLab2/NETLab2.TCPGenerator.Shared/TCPHeader.cs
+++ NETLab2/NETLab2.TCPGenerator.Shared/TCPHeader.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace NETLab2.TCPGenerator.Shared
@@ -22,52 +23,94 @@ namespace NETLab2.TCPGenerator.Shared
     /// <summary>
     /// Класс-заголовок пакета TCP протокола
     /// </summary>
+    [Serializable()]
     class TCPHeader
     {
         /// <summary>
         /// Порт отправителя
         /// </summary>
-        ushort _src_port { get; set; }
+        ushort _src_port;
+        public ushort SrcPort
+        {
+            set { _src_port = value; }
+        }
 
         /// <summary>
         /// Порт получателя 
         /// </summary>
-        ushort _dst_port { get; set; }
+        ushort _dst_port;
+        public ushort DstPort
+        {
+            set { _dst_port = value; }
+        }
 
         /// <summary>
         /// Номер очереди
         /// </summary>
-        uint _seq_n { get; set; }
+        uint _seq_n;
+        public uint SeqN
+        {
+            set { _seq_n = value; }
+            get { return _seq_n; }
+        }
 
         /// <summary>
         /// Номер подтверждения
         /// </summary>
-        uint _ack_n { get; set; }
+        uint _ack_n;
+        public uint AckN
+        {
+            set { _ack_n = value; }
+            get { return _ack_n; }
+        }
 
         /// <summary>
         /// Смещение данных (4 бита) + Зарезервировано (4 бита)
         /// </summary>
-        byte _offset { get; set; }
+        byte _offset;
+        public byte Offset
+        {
+            set { _offset = value; }
+        }
 
         /// <summary>
         /// Зарезервировано (2 бита) + Флаги (6 бит)
         /// </summary>
-        byte _flags { get; set; }
+        byte _flags;
+        public byte Flags
+        {
+            set { _flags = value; }
+            get { return _flags; }
+        }
 
         /// <summary>
         /// Размер окна
         /// </summary>
-        ushort _win { get; set; }
+        ushort _win;
+        public ushort Win
+        {
+            set { _win = value; }
+            get { return _win; }
+        }
 
         /// <summary>
         /// Контрольная сумма заголовка
         /// </summary>
-        ushort _crc { get; set; }
+        ushort _crc;
+        public ushort Crc
+        {
+            set { _crc = value; }
+            get { return _crc; }
+        }
 
         /// <summary>
         /// Дополнение до 20 байт
         /// </summary>
-        ushort _padding { get; set; }
+        ushort _padding;
+        public ushort Padding
+        {
+            set { _padding = value; }
+        }
 
         /// <summary>
         /// Пустой коструктор класса TCPHeader
@@ -112,8 +155,8 @@ namespace NETLab2.TCPGenerator.Shared
                 _seq_n = (uint)rand.Next();
             if (fin == true)
                 _flags |= (byte)TcpFlags.FIN;
-            _win = (ushort)Math.Pow(2, rand.Next(11));
-            _offset = (byte)Marshal.SizeOf(new TCPHeader());
+            _win = (ushort)Math.Pow(2, 5 + rand.Next(6));
+            _offset = (byte)(0xF0 & ((Marshal.SizeOf(new PseudoHeader()) / 4) << 4));
             rs_pseudo_crc(data, data.Length, _src_port, _dst_port, _offset + data.Length, 6);
             _padding = 0;
         }
@@ -121,7 +164,7 @@ namespace NETLab2.TCPGenerator.Shared
         /// <summary>
         /// Класс-псевдоструктура заголовка пакета TCP протокола для вычисления контрольной суммы
         /// </summary>
-        class PseudoHeader
+        struct PseudoHeader
         {
             /// <summary>
             /// Адрес отправителя
@@ -195,6 +238,59 @@ namespace NETLab2.TCPGenerator.Shared
             buffer = new List<byte>(Utils.SerializeMessage(ph));
             buffer.InsertRange(header_length, Encoding.Unicode.GetBytes(data));
             _crc = rs_crc(buffer, full_length);
+        }
+
+        public void Send(Socket socket, string data, int data_length, string receiver_ip, string dst_port_raw)
+        {
+            List<byte> buffer = new List<byte>(Utils.ObjectToByteArray(this));
+            foreach (var ch in data)
+                buffer.Add((byte)ch);
+            try
+            {
+                EndPoint remote = (EndPoint)(new IPEndPoint(IPAddress.Parse(receiver_ip), int.Parse(dst_port_raw)));
+                socket.SendTo(buffer.ToArray(), remote);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Deserialization constructor
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="ctxt"></param>
+        public TCPHeader(SerializationInfo info, StreamingContext ctxt)
+        {
+            //Get the values from info and assign them to the appropriate properties
+            _src_port = (ushort)info.GetValue("SrcPort", typeof(ushort));
+            _dst_port = (ushort)info.GetValue("DstPort", typeof(ushort));
+            _seq_n = (uint)info.GetValue("SeqN", typeof(uint));
+            _ack_n = (uint)info.GetValue("AckN", typeof(uint));
+            _offset = (byte)info.GetValue("Offset", typeof(byte));
+            _flags = (byte)info.GetValue("Flags", typeof(byte));
+            _win = (ushort)info.GetValue("Win", typeof(ushort));
+            _crc = (ushort)info.GetValue("Crc", typeof(ushort));
+            _padding = (ushort)info.GetValue("Padding", typeof(ushort));
+        }
+
+        /// <summary>
+        /// Serialization function
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="ctxt"></param>
+        public void GetObjectData(SerializationInfo info, StreamingContext ctxt)
+        {
+            info.AddValue("SrcPort", _src_port);
+            info.AddValue("DstPort", _dst_port);
+            info.AddValue("SeqN", _seq_n);
+            info.AddValue("AckN", _ack_n);
+            info.AddValue("Offset", _offset);
+            info.AddValue("Flags", _flags);
+            info.AddValue("Win", _win);
+            info.AddValue("Crc", _crc);
+            info.AddValue("Padding", _padding);
         }
     }
 }
