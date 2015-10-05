9d6de91ef43d3532050c130d186387bf42301a34
Иван Еленский <elenskivan@hotmail.com>
2015-10-05 19:22:24 +0000

    Stable

---
+++ NETLab2/NETLab2.TCPGenerator.Shared/Utils.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace NETLab2.TCPGenerator.Shared
+{
+    class Utils
+    {
+        public static Byte[] SerializeMessage<T>(T msg) where T : class
+        {
+            int objsize = Marshal.SizeOf(typeof(T));
+            Byte[] ret = new Byte[objsize];
+            IntPtr buff = Marshal.AllocHGlobal(objsize);
+            Marshal.StructureToPtr(msg, buff, true);
+            Marshal.Copy(buff, ret, 0, objsize);
+            Marshal.FreeHGlobal(buff);
+            return ret;
+        }
+    }
+}
--- NETLab2/NETLab2.TCPGenerator.WPF/MainWindow.xaml.cs
+++ NETLab2/NETLab2.TCPGenerator.WPF/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NETLab2.TCPGenerator.Shared;
+using System;
+using System.Net;
+using System.Net.Sockets;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NETLab2.TCPGenerator.WPF
 {
@@ -20,14 +11,36 @@ namespace NETLab2.TCPGenerator.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private TCPHeader _header;
+        private Socket _socket;
+        private EndPoint _endPoint;
         public MainWindow()
         {
             InitializeComponent();
+
+            App.Current.Exit += Current_Exit;
+            SenderAddressBox.Text = Dns.GetHostAddresses(Dns.GetHostName())[0].MapToIPv4().ToString();
+        }
+
+        private void Current_Exit(object sender, ExitEventArgs e)
+        {
+            if (_socket != null)
+                _socket.Close();
         }
 
         private void GenerateButton_Click(object sender, RoutedEventArgs e)
         {
-
+            try
+            {
+                _socket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+                _endPoint = new IPEndPoint(IPAddress.Parse(ReceiverAddressBox.Text), int.Parse(ReceiverPortBox.Text));
+                _header = new TCPHeader(SenderPortBox.Text, ReceiverPortBox.Text, AckOut.IsChecked,
+                    PshOut.IsChecked, RstOut.IsChecked, SynOut.IsChecked, FinOut.IsChecked, Message.Text);
+                SendButton.IsEnabled = true;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Некорректные аргументы", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
 
         private void SendButton_Click(object sender, RoutedEventArgs e)
